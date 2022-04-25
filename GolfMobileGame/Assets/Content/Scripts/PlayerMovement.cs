e3faa7868e8ee55a7190b10a106648261c9a8a84
KarolNowak123 <81412845+KarolNowak123@users.noreply.github.com>
2022-04-25 15:18:40 +0200

    Improved player movement and physicMaterial.

--- GolfMobileGame/Assets/Content/Scripts/PlayerMovement.cs
+++ GolfMobileGame/Assets/Content/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private Touch theTouch;
     private Vector2 startPosition, endPosition;
     private Vector3 shootPlayer;
-    private bool movePlayer;
+    private bool movePlayer = false;
     private Vector3 previousPosition;
 
     [SerializeField]
@@ -23,32 +23,41 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount>0 && playerTransform.velocity.x == 0 && playerTransform.velocity.z == 0)
+        if (Input.touchCount>0 && playerTransform.velocity.x < 0.02 && playerTransform.velocity.z < 0.02)
         {
-            theTouch = Input.GetTouch(0);
-            if(theTouch.phase == TouchPhase.Began)
-            {
-                startPosition = theTouch.position;
-            }
-            if(theTouch.phase == TouchPhase.Ended)
-            {
-                Debug.Log(movePlayer);
-                endPosition = theTouch.position;
-                movePlayer = true;
-            }
+            movePlayer = TouchControler();
         }
-        
-        if(movePlayer)
+        if (movePlayer)
         {
-            shootPlayer.x = (endPosition.y - startPosition.y) ;
+            MovePlayer();
+            movePlayer = false;
+        }
+     }
+
+    void MovePlayer()
+    {
+        
+            shootPlayer.x = (endPosition.y - startPosition.y);
             shootPlayer.z = (endPosition.x - startPosition.x) * -1;
             shootPlayer.y = 0;
-
-            playerTransform.AddForce(shootPlayer *-1 * moveSpeed);
-            movePlayer = false;
+            playerTransform.AddForce(shootPlayer * -1 * moveSpeed);
             //playerTransform.AddForce += (endPosition.y - startPosition.y) * moveSpeed * Time.deltaTime;
+    }
+    bool TouchControler()
+    {
+
+        theTouch = Input.GetTouch(0);
+        if (theTouch.phase == TouchPhase.Began)
+        {
+            startPosition = theTouch.position;
+        }
+        if (theTouch.phase == TouchPhase.Ended)
+        {
+            //Debug.Log(movePlayer);
+            endPosition = theTouch.position;
+            movePlayer = true;
         }
 
-        //Debug.Log(startPosition);
+        return movePlayer;
     }
 }
